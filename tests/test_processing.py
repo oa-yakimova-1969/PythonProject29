fe2874720056b5ab10a29d8ddab154a7b1eb7702
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-16 17:18:13 +0300

    изменения в test_masks

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,39 +1,45 @@
 import pytest
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+
+from src.processing import filter_by_state, sort_by_date
+
 
 @pytest.fixture
-def transactions():
+def transactions() -> list[dict]:
     return [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-def test_filter_by_state(transactions, state='EXECUTED'):
-    assert filter_by_state(transactions, state='EXECUTED') == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
-]
-
-def test_filter_by_state_canceled(transactions, state = 'CANCELED'):
-    assert filter_by_state(transactions, state = 'CANCELED') == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
-]
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-def test_sort_by_date(transactions, reverse = True):
-    assert sort_by_date(transactions, reverse = True) == [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-]
-def test_sort_by_date_false(transactions, reverse = False):
-    assert sort_by_date(transactions, reverse = False) == [
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
-]
+def test_filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> None:
+    assert filter_by_state(transactions, state="EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state_canceled(transactions: list[dict], state: str = "CANCELED") -> None:
+    assert filter_by_state(transactions, state="CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_sort_by_date(transactions: list[dict], reverse: bool = True) -> None:
+    assert sort_by_date(transactions, reverse=True) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_sort_by_date_false(transactions: list[dict], reverse: bool = False) -> None:
+    assert sort_by_date(transactions, reverse=False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
