d93765a708c56b0900e7580383142122f87f2adb
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-16 06:59:46 +0300

    В модуле test_masks создали функцию test_get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 from src.masks import get_mask_card_number
-
+from src.masks import get_mask_account
 
 
 @pytest.fixture
@@ -15,3 +15,17 @@ def test_get_mask_card_number_incorrect_format():
     assert get_mask_card_number("Abcd@#$%;:[}") == "Некорректный ввод"
 def test_get_mask_card_number_empty():
     assert get_mask_card_number("") == "Некорректный ввод"
+
+
+@pytest.fixture
+def account():
+    return 99998888777766665555
+
+def test_get_mask_account(account):
+    assert get_mask_account(account) == '**5555'
+def test_get_mask_account_incorrect_length():
+    assert get_mask_account(8888777766665555) == "Некорректный ввод"
+def test_get_mask_account_incorrect_format():
+    assert get_mask_account("Abcd@#$%;:[}<>?!") == "Некорректный ввод"
+def test_get_mask_account_empty():
+    assert get_mask_account("") == "Некорректный ввод"
