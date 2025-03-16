cad53147355da99e602d61856c9810349723cc01
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-16 07:25:06 +0300

    В модуле test_processing создали функцию test_filter_by_state

---
+++ tests/test_processing.py
@@ -0,0 +1,25 @@
+import pytest
+from src.processing import filter_by_state
+#from src.processing import sort_by_date
+
+@pytest.fixture
+def transactions():
+    return [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+def test_filter_by_state(transactions, state='EXECUTED'):
+    assert filter_by_state(transactions, state='EXECUTED') == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+]
+
+def test_filter_by_state_canceled(transactions, state = 'CANCELED'):
+    assert filter_by_state(transactions, state = 'CANCELED') == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+]
+
+
